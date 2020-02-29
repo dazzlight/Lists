426d77fea40f7ada19af74a3a2b9ef1cee5f31b5
dazzlight <adelina08042004@gmail.com>
2020-02-29 12:59:43 +0200

    Tested JavaScript

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,25 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+const a = 7;
+const b = 3;
+const plus = a + b;
+const minus = a - b;
+const multiply = a * b;
+const divide = a / b;
+
+console.log(plus);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+
+// console.log("Desktop -> Electron");
+// console.log("Smartphone & Tablet -> Cordova");
+// console.log("Server -> Node.js");
+// console.log("---History---");
+// console.info("1995 - Netscape");
+// console.info("1997 - ECMA Standart");
+// console.log("Ecma Script = JavaScript");
+// console.log("1999 - EcmaScript 3");
+// console.log("2009 - EcmaScript 5");
+// console.log("2015 - EcmaScript 2015 -> ES6");
+// console.log(">2015 - new releases");
